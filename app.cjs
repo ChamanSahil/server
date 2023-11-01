fc7eb13b975cf4725215ccc37d559c67b6c5056d
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-01 07:54:03 +0530

    Create app.cjs

---
+++ app.cjs
@@ -0,0 +1,30 @@
+"use strict";
+
+let cors = require('cors');
+
+// Importing dependencies and setting up http server
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default,
+  app = express().use(body_parser.json());
+
+console.log("In the file");
+
+app.use(cors());
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+// app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+app.get("/", async(req, res) => {
+  console.log("Root endpoint")
+  res.send("Great stuff, will win this for sure :)")
+})
+
+app.get("/test", async(req, res) => {
+  console.log("Test endpoint")
+  res.send("India is the best place and I am going to win this for sure")
+})
