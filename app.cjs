3f3830669d070d1f0416a3b627ded30e399fb02c
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-08 16:23:49 +0530

    Update app.cjs

--- app.cjs
+++ app.cjs
@@ -2,6 +2,16 @@
 
 let cors = require('cors');
 
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_IP,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+  },
+});
+
 // Importing dependencies and setting up http server
 const request = require("request"),
   express = require("express"),
@@ -20,10 +30,18 @@ console.log('Server running on http://%s:%s', ip, port);
 // app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 app.get("/", async(req, res) => {
+  const users = await knex("e_users").select("*");
+  if(!users.length) {
+    res.send(`Great stuff, ${process.env['name']}. You will win ${process.env['hackathon']} for sure :)`)
+  } else {
+    console.log(users)
+    console.log(users.length)
+    res.send(`Great stuff, ${users[0].name} : ${process.env['name']}. You will win ${process.env['hackathon']} for sure :)`)
+  }
+  
   console.log("Root endpoint")
   console.log(process.env['name'])
   console.log(process.env['hackathon'])
-  res.send(`Great stuff, ${process.env['name']} will win ${process.env['hackathon']} for sure :)`)
 })
 
 app.get("/test", async(req, res) => {
