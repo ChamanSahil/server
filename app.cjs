ea1615db134f778b2b79c29f2c7df0bfa9283e3d
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-12 13:34:36 +0530

    Update app.cjs

--- app.cjs
+++ app.cjs
@@ -517,25 +517,24 @@ app.post("/login", async(req, res) => {
 
 // function to get the output of the openshift AI model:
 app.post("/getMLOutput", async(req, res) => {
-    console.log(req.body.url)
-    // let result = await fetch(req.body.url)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         console.log(`Get ML Output is: ${data}`)
-    //         return data
-    //     })
+    let result = await fetch(req.body.url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(`Get ML Output is: ${data}`)
+            return data
+        })
 
     let returnString = ""
     
     // Only a single key-value object will be returned from the flask application
-    // Object.entries(result).forEach(([key, value]) => {
-    //     if(key != "Error") {
-    //         console.log(`${key.toUpperCase()} : ${(value*100).toFixed(1)} %`)
-    //         returnString = `${key.toUpperCase()} : ${(value*100).toFixed(1)} %`
-    //     } 
-    // })
+    Object.entries(result).forEach(([key, value]) => {
+        if(key != "Error") {
+            console.log(`${key.toUpperCase()} : ${(value*100).toFixed(1)} %`)
+            returnString = `${key.toUpperCase()} : ${(value*100).toFixed(1)} %`
+        } 
+    })
 
-    res.send(`${req.body.url}`)
+    res.send(returnString)
 })
 
 app.post("/addProduct", async(req, res) => {
